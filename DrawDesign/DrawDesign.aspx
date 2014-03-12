3161f3317cd7b7209d0ddcee246bf4373c212c46
iCabanne <isabel@littleraft.com>
2014-03-12 11:57:06 -0500

    14-03-12 Draw Design and Search

---
+++ DrawDesign/DrawDesign.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="DrawDesign.aspx.vb" Inherits="DrawDesign_DrawDesign" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        table { border-collapse: collapse; }
+        td { border: solid 1px pink; width:40px; }
+        td.bb { border-bottom: solid 1px #333; }
+        td.br { border-right: solid 1px #333; }
+        td.r1 {background-color:Gray}
+        td.r2 {background-color:Aqua}
+        td.r3 {background-color:Fuchsia;}
+        td.r4 {background-color:Teal;}
+        td.r5 {background-color:Yellow;}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:PlaceHolder ID="phDraw" runat="server"></asp:PlaceHolder>
+    </div>
+    </form>
+</body>
+</html>
---
+++ DrawDesign/DrawDesign.aspx.vb
@@ -0,0 +1,100 @@
+﻿
+Partial Class DrawDesign_DrawDesign
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        BuildDraw()
+    End Sub
+    Protected Sub BuildDraw()
+        Dim tbl As New HtmlTable
+        Dim nRow As Integer = 0
+        Dim nCell As Integer = 0
+        Dim ntxtnum As Integer = 0
+        Dim sClass As String = ""
+        With tbl
+            For nRowCnt As Integer = 0 To 63
+                .Rows.Add(New HtmlTableRow)
+                nRow = .Rows.Count - 1
+                With .Rows(nRow)
+                    For nCellCnt As Integer = 0 To 20
+                        .Cells.Add(New HtmlTableCell)
+                        nCell = .Cells.Count - 1
+                        With .Cells(nCell)
+                            If nCell = 0 Then
+                                .InnerText = IIf(((nRow + 1) / 2) = Int((nRow + 1) / 2), _
+                                                 ((nRow + 1) / 2).ToString, _
+                                                 ".")
+                            Else
+                                '.InnerText = (Int((nCell - 1) / 4)).ToString
+                                '.InnerText = Int((nRow + 7) / 16)
+                            End If
+                            If nRow > 0 Then
+                                sClass = ""
+                                '
+                                ' Border bottom & round
+                                If nCell < 5 Then
+                                    If ((nRow + 0) / 2) <> Int((nRow + 0) / 2) Then
+                                        sClass = sClass & "bb r" & Int((nCell + 3) / 4).ToString
+                                    End If
+                                    If ((nRow + 0) / 2) <> Int((nRow + 0) / 2) Then
+                                    End If
+                                ElseIf nCell < 9 Then
+                                    If ((nRow + 2) / 4) = Int((nRow + 2) / 4) Then
+                                        sClass = sClass & "bb r" & Int((nCell + 3) / 4).ToString
+                                    End If
+                                    If ((nRow + 2) / 4) = Int((nRow + 2) / 4) Then
+                                    End If
+                                ElseIf nCell < 13 Then
+                                    If ((nRow + 4) / 8) = Int((nRow + 4) / 8) Then
+                                        sClass = sClass & "bb r" & Int((nCell + 3) / 4).ToString
+                                    End If
+                                    If ((nRow + 4) / 8) = Int((nRow + 4) / 8) Then
+                                    End If
+                                ElseIf nCell < 17 Then
+                                    If ((nRow + 8) / 16) = Int((nRow + 8) / 16) Then
+                                        sClass = sClass & "bb r" & Int((nCell + 3) / 4).ToString & " "
+                                    End If
+                                ElseIf nCell < 21 Then
+                                    If ((nRow + 16) / 32) = Int((nRow + 16) / 32) Then
+                                        sClass = sClass & "bb r" & Int((nCell + 3) / 4).ToString & " "
+                                    End If
+                                End If
+                                '
+                                ' Border Right
+                                If ((nCell / 4) = Int(nCell / 4)) Then
+                                    If Int(nCell / 4) = 1 Then
+                                        If ((Int((nRow + 0) / 2)) / 2) <> Int((Int((nRow + 0) / 2)) / 2) Then
+                                            sClass = sClass & " br"
+                                        End If
+                                    ElseIf Int(nCell / 4) = 2 Then
+                                        If ((Int((nRow + 1) / 4)) / 2) <> Int((Int((nRow + 1) / 4)) / 2) Then
+                                            sClass = sClass & " br"
+                                        End If
+                                    ElseIf Int(nCell / 4) = 3 Then
+                                        If ((Int((nRow + 3) / 8)) / 2) <> Int((Int((nRow + 3) / 8)) / 2) Then
+                                            sClass = sClass & " br"
+                                        End If
+                                    ElseIf Int(nCell / 4) = 4 Then
+                                        If ((Int((nRow + 7) / 16)) / 2) <> Int((Int((nRow + 7) / 16)) / 2) Then
+                                            sClass = sClass & " br"
+                                        End If
+                                    ElseIf Int(nCell / 4) = 5 Then
+                                        If ((Int((nRow + 15) / 32)) / 2) <> Int((Int((nRow + 15) / 32)) / 2) Then
+                                            sClass = sClass & " br"
+                                        End If
+                                    End If
+                                End If
+                                '
+                                sClass = Trim(sClass)
+                                If Len(sClass) > 0 Then
+                                    .Attributes.Add("class", sClass)
+                                End If
+                            End If
+                        End With
+                    Next
+                End With
+            Next
+        End With
+        phDraw.Controls.Add(tbl)
+    End Sub
+End Class
